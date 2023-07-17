493961f107d007862f956a4668fc2642a8ac1bb1
RahmanIslamIen <rahmanislamien@gmail.com>
2023-07-17 12:12:27 +0700

    bagian breadcrumb

--- app/Views/admin/menu_pengumuman.php
+++ app/Views/admin/menu_pengumuman.php
@@ -1,5 +1,25 @@
 <?= $this->extend('layout/page_layout') ?>
 
+<?= $this->section('breadcrumb') ?>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1 class="m-0">Dashboard</h1>
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Pengumuman</li>
+        </ol>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+<?= $this->endSection() ?>
+
 <?= $this->section('content') ?>
 <button type="button" class="btn btn-success m-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
   Tambah Pengumuman Baru
