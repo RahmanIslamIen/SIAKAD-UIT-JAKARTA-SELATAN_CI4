4be39690d007ac5dd420f77d199531fc05f8e9ac
RahmanIslamIen <rahmanislamien@gmail.com>
2023-08-01 19:14:31 +0700

    koding model rekap nilai

--- app/Models/RekapNilaiModel.php
+++ app/Models/RekapNilaiModel.php
@@ -2,3 +2,9 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+
+class RekapNilaiModel extends Model{
+    protected $table = 'rekap_nilai';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['matakuliah', 'nilai', 'pertemuan', 'jenis_penilaian'];
+}
